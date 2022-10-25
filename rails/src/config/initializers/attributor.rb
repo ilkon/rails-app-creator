bb57081b052937f57a73c2ccae36cbda91e1923d
Ilya Konyukhov <ilya@konyukhov.com>
2022-10-25 15:22:50 -0400

    Removing attributor

--- rails/src/config/initializers/attributor.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-require 'attributor'
-
-Attributor.configure do |config|
-  config.stretches = Rails.env.test? ? 1 : 11
-  config.pepper = ENV.fetch('PEPPER', nil)
-end
