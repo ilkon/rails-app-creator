f26199e6989e23b0c25f2b614eaf1baff55b0ff7
Ilya Konyukhov <ilya@konyukhov.com>
2022-10-29 20:46:50 -0400

    Fixing logging

--- rails/src/config/initializers/logging.rb
+++ rails/src/config/initializers/logging.rb
@@ -4,7 +4,7 @@ if defined?(Rails::Console)
   Rails.application.config.logger = Logger.new('/dev/null')
   Rails.application.config.after_initialize { ActiveRecord::Base.logger.level = 0 }
 
-elsif defined?(Lograge)
+elsif defined?(Lograge) && !Rails.env.test?
   Rails.application.config.log_level = ENV.fetch('LOG_LEVEL', 'debug').upcase
   Rails.application.config.log_tags = [:remote_ip]
 
