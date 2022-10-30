bd2937b229e2f140c2c43f4fb93659b60315da1f
Ilya Konyukhov <ilya@konyukhov.com>
2022-10-29 20:40:12 -0400

    Fixing logging

--- rails/src/config/initializers/logging.rb
+++ rails/src/config/initializers/logging.rb
@@ -3,31 +3,29 @@
 if defined?(Rails::Console)
   Rails.application.config.logger = Logger.new('/dev/null')
   Rails.application.config.after_initialize { ActiveRecord::Base.logger.level = 0 }
+
 elsif defined?(Lograge)
   Rails.application.config.log_level = ENV.fetch('LOG_LEVEL', 'debug').upcase
   Rails.application.config.log_tags = [:remote_ip]
 
-  Rails.application.config.logger = ActiveSupport::TaggedLogging.new(ActiveSupport::Logger.new($stdout).tap do |logger|
-    logger.level = Rails.application.config.log_level
-  end)
+  Rails.application.config.logger = ActiveSupport::TaggedLogging.new(ActiveSupport::Logger.new($stdout, level: Rails.application.config.log_level))
 
   Rails.application.config.lograge.custom_options = lambda do |event|
     data = event.payload
 
     addons = {
       env:       Rails.env,
-      host:      ENV.fetch('HOST', nil) || 'localhost',
+      host:      ENV.fetch('HOST', 'localhost'),
       timestamp: Time.current
     }
 
     addons[:error] = { type: data[:exception], content: data[:exception_object] } if data[:exception].present?
-
-    reject_keys = %w[controller action]
-    addons[:params] = data[:params].except(*reject_keys) if data[:params].present?
+    addons[:params] = data[:params].except(%w[controller action]) if data[:params].present?
 
     addons
   end
 
+  Rails.application.config.lograge.ignore_actions = ['HealthcheckController#index']
   Rails.application.config.lograge.formatter = Lograge::Formatters::KeyValue.new
   Rails.application.config.lograge.enabled = true
 end
