bcd122be9f7b0e008ec14fa7acf4356ac3236d72
Ilya Konyukhov <ilya@konyukhov.com>
2022-12-07 13:57:21 -0500

    Fixing rollbar config

--- rails/src/config/initializers/rollbar.rb
+++ rails/src/config/initializers/rollbar.rb
@@ -2,30 +2,33 @@
 
 if defined?(Rollbar)
   Rollbar.configure do |config|
-    config.access_token = ENV.fetch('ROLLBAR_ACCESS_TOKEN', '')
-    config.enabled = Rails.env.staging? || Rails.env.production?
-    config.send_extra_frame_data = :app
-    # config.person_method = 'user'
-    # config.person_id_method = "my_id"
-    config.person_username_method = 'email'
-    # config.person_email_method = "my_email"
-    # config.custom_data_method = lambda { {:some_key => "some_value" } }
+    config.environment = Rails.env
 
-    if defined?(Sidekiq)
-      config.use_sidekiq 'queue' => 'default'
-      config.failover_handlers = [Rollbar::Delay::Thread]
-    else
-      config.async_handler = proc do |payload|
-        Thread.new { Rollbar.process_from_async_handler(payload) }
+    config.enabled = (Rails.env.staging? || Rails.env.production?) && ENV.fetch('ROLLBAR_ACCESS_TOKEN').present?
+    if config.enabled
+      config.access_token = ENV.fetch('ROLLBAR_ACCESS_TOKEN')
+
+      config.send_extra_frame_data = :app
+      # config.person_method = 'user'
+      # config.person_id_method = "my_id"
+      config.person_username_method = 'email'
+      # config.person_email_method = "my_email"
+      # config.custom_data_method = lambda { {:some_key => "some_value" } }
+
+      if defined?(Sidekiq)
+        config.use_sidekiq 'queue' => 'default'
+        config.failover_handlers = [Rollbar::Delay::Thread]
+      else
+        config.async_handler = proc do |payload|
+          Thread.new { Rollbar.process_from_async_handler(payload) }
+        end
       end
     end
 
-    config.environment = Rails.env
-
-    config.js_enabled = config.enabled
+    config.js_enabled = config.enabled && ENV.fetch('ROLLBAR_CLIENT_ACCESS_TOKEN').present?
     if config.js_enabled
       config.js_options = {
-        accessToken:                ENV.fetch('ROLLBAR_CLIENT_ACCESS_TOKEN', ''),
+        accessToken:                ENV.fetch('ROLLBAR_CLIENT_ACCESS_TOKEN'),
         captureUncaught:            true,
         captureUnhandledRejections: true,
         payload:                    {
